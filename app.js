a8dd06d83479c5f6c845746dc362e411f5b63a21
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2025-11-17 18:36:41 +0700

    add flutter app to web content

--- app.js
+++ app.js
@@ -4,4 +4,30 @@ window.onload = function () {
     const selectElement = document.getElementById('color-dropdown');
     selectElement.value = 'red';
 
+    // START FLUTTER BUILD
+
+    // Get the embedding target element
+    const embeddingTarget = document.querySelector("#embed");
+
+    // Setup mainJsPath for run flutter web
+    _flutter.buildConfig = {
+        "builds": [
+            {
+                "mainJsPath": "./flutter/build/web/main.dart.js",
+            }
+        ]
+    };
+
+    // Setup asset base where the flutter app folder exist
+    // and put the app on embedding target
+    const config = {
+        "assetBase": "./flutter/build/web/",
+        "hostElement": embeddingTarget,
+    };
+
+    // Load the flutter app
+    _flutter.loader.load({
+        config: config,
+    });
+    // END FLUTTER BUILD
 }
