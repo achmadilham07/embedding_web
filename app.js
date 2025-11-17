b4be579963929f97a83f84945e7c16eb95f83ec8
Achmad Ilham <30348815+achmadilham07@users.noreply.github.com>
2025-11-17 18:31:08 +0700

    add html-css-js project

---
+++ app.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+
+    // Set default selected value to "red"
+    const selectElement = document.getElementById('color-dropdown');
+    selectElement.value = 'red';
+
+}
